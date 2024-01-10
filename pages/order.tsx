fe819d78057d922ea16dccc5144fa53b8e418f03
daksh <akshd@outlook.com>
2024-01-10 22:46:53 +0530

    chore: rename for deployment

--- pages/order.tsx
+++ pages/order.tsx
@@ -1,8 +1,8 @@
-import Order from '@/components/Order.page';
+import OrderComponent from '@/components/Order.page';
 import Scanner from '@/components/Scanner.component';
 import { useState } from 'react'
 
-const order = () => {
+const Order = () => {
     const [data, setData] = useState<String | null>(null);
 
 
@@ -12,11 +12,11 @@ const order = () => {
                 !data ? (
                     <Scanner data={data} setData={setData} />
                 ) : (
-                    <Order table={data} />
+                    <OrderComponent table={data} />
                 )
             }
         </div>
     )
 }
 
-export default order;
+export default Order;
