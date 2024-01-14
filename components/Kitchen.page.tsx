ca90b71a646f33f359a961f389d7448f50f2c481
daksh <akshd@outlook.com>
2024-01-14 23:31:35 +0530

    chore: update dummy data

--- components/Kitchen.page.tsx
+++ components/Kitchen.page.tsx
@@ -18,6 +18,19 @@ const Kitchen = () => {
                 }
             ],
         },
+        {
+            table: "B-23",
+            order: [
+                {
+                    title: "Pasta",
+                    price: 130,
+                },
+                {
+                    'title': 'Burger',
+                    'price': 200
+                }
+            ],
+        },
     ];
     return (
         <div className="flex items-center justify-between p-8 flex-col">
