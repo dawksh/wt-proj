8536cf3e921c64ab17968a1fedcdfb24073a4f5e
daksh <akshd@outlook.com>
2024-01-24 22:14:01 +0530

    feat: add cart

--- components/Item.component.tsx
+++ components/Item.component.tsx
@@ -1,13 +1,18 @@
-import React from 'react'
+import React, { Dispatch, SetStateAction } from 'react'
 import { Button } from './ui/button'
+import { Order } from '@/lib/types'
 
-const Item = ({ title, price }: { title: String, price: Number }) => {
+const Item = ({ title, price, setOrder }: { title: String, price: Number, setOrder: Dispatch<SetStateAction<Order[]>> }) => {
     return (
         <div className='w-full h-64 my-8 flex items-center justify-center border-2 border-slate-700 flex-col rounded-md'>
             {title}
             <br />
             Rs. {price.toString()}
-            <Button className='my-2'>Add to cart</Button>
+            <Button className='my-2' onClick={(e) => {
+                setOrder((prev: any) => {
+                    return [...prev, { title, price }]
+                })
+            }}>Add to cart</Button>
         </div>
     )
 }
--- components/Order.page.tsx
+++ components/Order.page.tsx
@@ -5,8 +5,20 @@ import {
     CarouselNext,
     CarouselPrevious,
 } from "@/components/ui/carousel"
+import {
+    Drawer,
+    DrawerClose,
+    DrawerContent,
+    DrawerDescription,
+    DrawerFooter,
+    DrawerHeader,
+    DrawerTitle,
+    DrawerTrigger,
+} from "@/components/ui/drawer"
+import { Button } from "./ui/button"
 import Item from "./Item.component"
 import { Order } from "@/lib/types"
+import { useState } from "react"
 
 const items: Array<Order> = [
     {
@@ -24,15 +36,49 @@ const items: Array<Order> = [
 ]
 
 const Order = ({ table }: { table: String }) => {
+    const [order, setOrder] = useState<Array<Order>>([]);
+    const renderCart = () => {
+        const total = order.reduce((acc, curr) => acc + Number(curr.price), 0)
+        return (
+            <div className="mx-4">
+                {order.map((item, index) => (
+                    <div key={index}>
+                        {item.title} - {item.price.toString()}
+                    </div>
+                ))}
+                <p className="font-semibold">Total: {total}</p>
+            </div>
+        )
+    }
+
     return (
         <div className='p-4'>
-            <h2 className='text-lg font-medium'>Table: {table}</h2>
+            <div className="flex flex-row justify-between">
+                <h2 className='text-lg font-medium'>Table: {table}</h2>
+                <Drawer>
+                    <Button><DrawerTrigger>View Cart</DrawerTrigger></Button>
+                    <DrawerContent>
+                        <DrawerHeader>
+                            <DrawerTitle>Cart</DrawerTitle>
+                        </DrawerHeader>
+                        <DrawerDescription>
+                            {renderCart()}
+                        </DrawerDescription>
+                        <DrawerFooter>
+                            <Button>Confirm</Button>
+                            <DrawerClose>
+                                <Button variant="outline">Close</Button>
+                            </DrawerClose>
+                        </DrawerFooter>
+                    </DrawerContent>
+                </Drawer>
+            </div>
             <div>
                 <Carousel>
                     <CarouselContent>
                         {items.map((item, index) => (
                             <CarouselItem key={index}>
-                                <Item title={item.title} price={item.price} />
+                                <Item title={item.title} price={item.price} setOrder={setOrder} />
                             </CarouselItem>
                         )
                         )}
@@ -40,6 +86,7 @@ const Order = ({ table }: { table: String }) => {
                     <CarouselNext />
                     <CarouselPrevious />
                 </Carousel>
+
             </div>
         </div>
     )
