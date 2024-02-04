008f8ffe353028d03f1b40c22f24e52e29956cdc
daksh <akshd@outlook.com>
2024-02-04 22:22:02 +0530

    chore: fix formatting

--- pages/api/orders.ts
+++ pages/api/orders.ts
@@ -1,80 +1,101 @@
 // pages/api/orders.ts
-import { NextApiRequest, NextApiResponse } from 'next'
-import dbConnect from '@/lib/utils'
-import Order,{ IOrder } from '../../models/Order'
-import User from '../../models/User'
-
+import { NextApiRequest, NextApiResponse } from "next";
+import dbConnect from "@/lib/utils";
+import Order, { IOrder } from "../../models/Order";
+import User from "../../models/User";
 
 export default async function handler(
-  
-    req: NextApiRequest,
-    res: NextApiResponse
+  req: NextApiRequest,
+  res: NextApiResponse
 ) {
-  
-    const { method } = req
+  const { method } = req;
 
-    await dbConnect()
-    switch (method) {
-        case 'POST':
-          try {
-            const  order  = req.body
-            // Find the user
-            console.log("kitchen = ",order[0].kitchen)
-            const user = await User.findOne({username:order[0].kitchen}).exec()
-            console.log("user",user)
-            if (!user) {
-              return res.status(400).json({ success: false, message: 'User not found' })
-            }
-    
-            // Create the orders
-            const createdOrders = await Order.insertMany(order.map((order: IOrder) => ({ ...order, user:user._id })))
-    
-            res.status(201).json({ success: true, data: createdOrders })
-          } catch (error) {
-            res.status(400).json({ success: false, error: error instanceof Error ? error.message : 'An error occurred' })
-          }
-          break
-    case 'GET':
-      try { 
-       
-
+  await dbConnect();
+  switch (method) {
+    case "POST":
+      try {
+        const order = req.body;
         // Find the user
-        const userId = "65b12e7bec15d121f7d6fa4d"
-        // Get the user's orders
-        const orders = await Order.find({ user: userId }).exec()
-          // Map _id to id for each order
-    const ordersWithId = orders.map(order => ({
-      id: order._id.toString(),
-      title: order.title,
-      price: order.price,
-      user: order.user,
-      // Include other properties here
-    }));
-        // console.log(ordersWithId)
-        res.status(200).json({ success: true,  data: ordersWithId })
+        console.log("kitchen = ", order[0].kitchen);
+        const user = await User.findOne({
+          username: order[0].kitchen,
+        }).exec();
+        console.log("user", user);
+        if (!user) {
+          return res
+            .status(400)
+            .json({ success: false, message: "User not found" });
+        }
+
+        // Create the orders
+        const createdOrders = await Order.insertMany(
+          order.map((order: IOrder) => ({ ...order, user: user._id }))
+        );
+
+        res.status(201).json({ success: true, data: createdOrders });
       } catch (error) {
-        res.status(400).json({ success: false, error: error instanceof Error ? error.message : 'An error occurred' })
+        res.status(400).json({
+          success: false,
+          error:
+            error instanceof Error
+              ? error.message
+              : "An error occurred",
+        });
       }
-      break
-      case 'DELETE':
-          try {
-            // Get the ID of the order to delete from the request
-            const { id } = req.body;
+      break;
+    case "GET":
+      try {
+        // Find the user
+        const userId = "65b12e7bec15d121f7d6fa4d";
+        // Get the user's orders
+        const orders = await Order.find({ user: userId }).exec();
+        // Map _id to id for each order
+        const ordersWithId = orders.map((order) => ({
+          id: order._id.toString(),
+          title: order.title,
+          price: order.price,
+          user: order.user,
+          // Include other properties here
+        }));
+        // console.log(ordersWithId)
+        res.status(200).json({ success: true, data: ordersWithId });
+      } catch (error) {
+        res.status(400).json({
+          success: false,
+          error:
+            error instanceof Error
+              ? error.message
+              : "An error occurred",
+        });
+      }
+      break;
+    case "DELETE":
+      try {
+        // Get the ID of the order to delete from the request
+        const { id } = req.body;
 
-            // Find the order and delete it
-            const deletedOrder = await Order.findByIdAndDelete(id);
+        // Find the order and delete it
+        const deletedOrder = await Order.findByIdAndDelete(id);
 
-            if (!deletedOrder) {
-              return res.status(400).json({ success: false, message: 'Order not found' });
-            }
+        if (!deletedOrder) {
+          return res
+            .status(400)
+            .json({ success: false, message: "Order not found" });
+        }
 
-            res.status(200).json({ success: true, data: deletedOrder });
-          } catch (error) {
-            res.status(400).json({ success: false, error: error instanceof Error ? error.message : 'An error occurred' });
-          }
-          break
-        default:
-            res.status(400).json({ success: false })
-            break
-    }
+        res.status(200).json({ success: true, data: deletedOrder });
+      } catch (error) {
+        res.status(400).json({
+          success: false,
+          error:
+            error instanceof Error
+              ? error.message
+              : "An error occurred",
+        });
+      }
+      break;
+    default:
+      res.status(400).json({ success: false });
+      break;
+  }
 }
